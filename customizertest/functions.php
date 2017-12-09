2c7b890d344bc88fe2f4f831fa2a82502d55c41f
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2017-12-09 19:36:14 +0000

    Ability to edit background Colour
    With live preview

--- customizertest/functions.php
+++ customizertest/functions.php
@@ -22,3 +22,50 @@ function cd_setup() {
 	) );
 
 }
+
+/**
+*
+ * The code for the customiser starts here
+ *
+ */
+add_action( 'customize_register', 'cd_customizer_settings' );
+
+/**
+ *
+ * Sets up the visible fields in the customiser and registers the settings and the controls
+ *
+ */
+function cd_customizer_settings( $wp_customize ) {
+
+  $wp_customize->add_section( 'cd_colors' , array(
+      'title'      => 'Colors',
+      'priority'   => 30,
+  ) );
+
+  $wp_customize->add_setting( 'background_color' , array(
+    'default'     => '#43C6E4',
+    'transport'   => 'refresh',
+  ) );
+
+  $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'background_color', array(
+    'label'        => 'Background Color',
+    'section'    => 'cd_colors',
+    'settings'   => 'background_color',
+  ) ) );
+
+}
+
+/**
+ *
+ * Applies the settings to our theme
+ *
+ */
+add_action( 'wp_head', 'cd_customizer_css');
+function cd_customizer_css()
+{
+    ?>
+         <style type="text/css">
+             body { background: #<?php echo get_theme_mod('background_color', '#43C6E4'); ?>; }
+         </style>
+    <?php
+}
