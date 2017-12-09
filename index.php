fb14ba2e0654595cb72e864fd239611d32f9d00a
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2017-12-09 19:20:16 +0000

    Initial Comit
    Adapted from
    https://gist.github.com/danielpataki/cbea8009110a48b5021725c0cd0ae323
    and https://premium.wpmudev.org/blog/wordpress-theme-customizer-guide/

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+<div id="container">
+
+<div id="intro">
+<h1><?php bloginfo( 'site_title' ) ?></h1>
+<h2><?php bloginfo( 'description' ) ?></h2>
+<a href="" class='button'>Come On In</a>
+</div>
+
+</div>
+<?php get_footer() ?>
